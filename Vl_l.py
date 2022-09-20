7e9173d476e301be84383b13f77d0cc913a164ff
chawongr <96721815+chawongr@users.noreply.github.com>
2022-09-21 02:10:36 +0700

    Mac

---
+++ Vl_l.py
@@ -0,0 +1,9 @@
+def isMax(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        mx = isMax(lst[1:])
+        return mx if mx > lst[0] else lst[0]
+    
+com = list(map(int,input('Enter Input : ').split(' ')))
+print('Max :',isMax(com))
