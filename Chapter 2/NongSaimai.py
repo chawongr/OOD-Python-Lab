b5edd37a79ecb5c9c2085b3e247dc845521d5a82
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2022-09-24 01:34:03 +0700

    MacBook

---
+++ Chapter 2/NongSaimai.py
@@ -0,0 +1,8 @@
+def hbd(age):
+    if age%2==0:
+        return 'saimai is just 20, in base '+str(age//2)+'!'  
+    else:
+        return 'saimai is just 21, in base '+str(age//2)+'!'
+      
+age = int(input('Enter year : '))     
+print(hbd(age))
