3e773fd75befd6fcb3b1fbe8a5f3227e1b6e4e1f
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2022-09-24 01:32:13 +0700

    MacBook

---
+++ Chapter 2/NewRange.py
@@ -0,0 +1,27 @@
+print('*** New Range ***')
+
+def rangeTip(start,end,step = 1.0):
+    list = []
+    list.append(start)
+    while start+step<end:
+        start+=step 
+        start=round(start,3) 
+        list.append(start)
+    return list
+start = 0.0
+step = 1.0
+a = list(map(float,input('Enter Input : ').split()))
+
+if len(a)==3:
+    start=a[0]
+    end=a[1]
+    step=a[2]
+
+if len(a)==2:
+    start = a[0]
+    end=a[1]
+
+if len(a)==1:
+    end=a[0]
+
+print(tuple(rangeTip(start,end,step)))
