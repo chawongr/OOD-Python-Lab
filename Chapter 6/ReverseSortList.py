e4c09223f0ee112c16947245fc660c1bd0d6470f
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2022-10-03 18:28:17 +0700

    Recursion

--- Chapter 6/ReverseSortList.py
+++ Chapter 6/ReverseSortList.py
@@ -1,18 +1,18 @@
-def isMax (ll, mx = 0,i = 0) :
-    if i== len(ll):
-        return mx
-    if int(ll[i]) > int(ll[mx]):
-        return isMax(ll,i,i+1)
+def findMax(lst):
+    if len(lst)==1:
+        return lst[0]
     else:
-        return isMax(ll,mx,i+1)
+        mx=findMax(lst[1:])
+        return mx if mx>lst[0] else lst[0]
 
-def isSort(ll, temp = []):
+def isSort(ll,temp=[]):
     if len(ll) == 0:
         return temp
     else:
-        temp.append(int(ll.pop(isMax(ll))))
+        mx = findMax(ll)
+        temp.append(int(mx))
+        ll.remove(mx)
         return isSort(ll,temp)
 
-com = list(map(int,input('Enter your List : ').split(',')))
-print('List after Sorted :',isSort(com))
-
+com = list(map(int,input('Enter Input : ').split(',')))
+print(isSort(com))
