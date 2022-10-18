bec0c50170751daa183ab8c10e1a9608fed116ca
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2022-10-18 19:44:24 +0700

    Tree 1 (Binary Search Tree)

---
+++ Chapter 7/FindPapa.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, data): 
+        self.data = data  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.data) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.data:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.data:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+                
+def printTree90(node, level = 0):
+    if node != None:
+        printTree90(node.right, level + 1)
+        print('     ' * level, node)
+        printTree90(node.left, level + 1)
+
+def father(r,data):
+    if data == r.data:
+        return f'None Because {data} is Root'
+    while True:
+        if not r.left and not r.right:
+            return "Not Found Data"
+        if data < r.data and data == r.left.data:
+            return r.data
+        elif data > r.data and data == r.right.data:
+            return r.data
+        if data < r.data:
+            r = r.left
+        elif data > r.data:
+            r = r.right
+
+tree = BinarySearchTree()
+data = input("Enter Input : ").split("/")
+for e in data[0].split():
+    tree.create(e)
+printTree90(tree.root)
+print(father(tree.root,data[1]))
