5c58d530c8ef565117e6f447d1b342d12350dbd5
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2022-11-21 05:31:29 +0700

    Sorting

---
+++ Chapter 9/somethingDrome.py
@@ -0,0 +1,50 @@
+def metaDrome(lst):
+    if lst[0]>=lst[1]:
+        return False
+    else:
+        if len(lst)==2:
+            return True
+        return metaDrome(lst[1:])
+
+def plainDrome(lst):
+    if lst[0]>lst[1]:
+        return False
+    elif lst[0]==lst[1]:
+        return True
+    elif len(lst)==2:
+            return True
+    return plainDrome(lst[1:])
+
+def kataDrome(lst):
+    if lst[0]<=lst[1]:
+        return False
+    else:
+        if len(lst)==2:
+            return True
+        return kataDrome(lst[1:])
+
+def nialpDrome(lst):
+    if lst[0]<lst[1]:
+        return False
+    elif lst[0]==lst[1]:
+        return True
+    elif len(lst)==2:
+            return True
+    return nialpDrome(lst[1:])
+
+def repDrome(lst):
+    if lst[0]!=lst[1]:
+        return False
+    else:
+        if len(lst)==2:
+            return True
+        return repDrome(lst[1:])
+
+com = list(map(int,input('Enter Input : ')))
+if repDrome(com):print('Repdrome')
+elif metaDrome(com):print('Metadrome')
+elif plainDrome(com):print('Plaindrome')
+elif kataDrome(com):print('Katadrome')
+elif nialpDrome(com):print('Nialpdrome')
+else:print('Nondrome')
+
